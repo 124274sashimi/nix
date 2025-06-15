0db4b54fbf1e9b5645ff24599a76c81840b138bc
Hubert Liu <hubertliu100@gmail.com>
2025-06-14 17:50:51 -0700

    fix iwlwifi crash

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -12,6 +12,13 @@
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ ];
   boot.kernelParams = [ "processor.max_cstate=5" "pcie_aspm=off" ];
+  boot.extraModprobeConfig = ''
+    options iwlwifi 11n_disable=1 swcrypto=0 bt_coex_active=0 power_save=0
+    options iwlmvm power_scheme=1 
+    options iwlwifi d0i3_disable=1 
+    options iwlwifi uapsd_disable=1 
+    options iwlwifi lar_disable=1
+  '';
 
   boot.extraModulePackages = [ ];
 
