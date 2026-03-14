4e79b56ba494cea0952d1fce474503ef08638332
Hubert Liu <hubertliu100@gmail.com>
2026-03-13 17:53:10 -0700

    nix fmt

--- modules/cuda.nix
+++ modules/cuda.nix
@@ -14,17 +14,15 @@
   # environment.variables.LD_LIBRARY_PATH = pkgs.stdenv.cc.cc.lib;
   # https://discourse.nixos.org/t/importerror-libstdc-so-6-cannot-open-shared-object-file-no-such-file-or-directory/41988/4
   environment.variables.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc
-      pkgs.cudaPackages.cudatoolkit
-      pkgs.cudaPackages.cudnn
+    pkgs.stdenv.cc.cc
+    pkgs.cudaPackages.cudatoolkit
+    pkgs.cudaPackages.cudnn
   ];
 
   # shellHook = ''
   #   # fixes libstdc++ issues and libgl.so issues
   #   LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/
   # '';
-
-
 
   # environment.systemPackages = with pkgs; [
   #   linuxPackages.nvidia_x11
